a352ec401fd958c00a3ef1493eac08ed93aa218b
miyacoz <z@zeppel.biz>
2022-04-10 18:27:19 +0900

    reordered the lines and added comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+// i shall complain if i'm not given the `foo` :)
+const Component: React.FC<{ foo: string }> = props => <div {...props} />
+
+// i also should complain in the situation, but :(
 const Styled = styled.div<{ foo: string }>`
   color: red;
 `
-
-const Component: React.FC<{ foo: string }> = props => <div {...props} />
 
 const App: React.FC = () => (
   <>
